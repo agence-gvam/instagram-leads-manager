ac21e059229502dfc75798c013e0b79d0d593e6b
Gary TELL <tell.gary.lucien@gmail.com>
2023-11-24 11:40:37 +0100

    Fixing some issues.

--- controllers/leads.controller.js
+++ controllers/leads.controller.js
@@ -2,32 +2,30 @@ const Lead = require("../models/lead.model");
 const { countLeads } = require("../services/counter.service");
 
 module.exports.createLead = async (req, res) => {
-  const splitedUrl = req.body.url.split("/");
-
-  let platform = splitedUrl[2].split(".")[1];
-  platform = platform.charAt(0).toUpperCase() + platform.slice(1);
-
-  const userName = splitedUrl[3];
-
-  const newUrl = `${splitedUrl[0]}//${splitedUrl[2]}/${splitedUrl[3]}`;
+  const { url } = req.body;
+  const splitedUrl = url.split("/");
 
   try {
-    if (!req.body.url) throw new Error("You must fill the input");
-
-    if (platform !== "Instagram")
+    if (!url) throw new Error("You must fill the input");
+    if (!url.includes("instagram"))
       throw new Error("This is not an Instagram link");
+    if (!url.includes("https://"))
+      throw new Error("Only HTTPS links are authorized");
+
+    const userName = splitedUrl[3];
+
+    const newUrl = `${splitedUrl[0]}//${splitedUrl[2]}/${splitedUrl[3]}`;
 
     let isLead = false;
     const lead = await Lead.findOne({ url: newUrl });
 
     if (lead) isLead = true;
-    else await Lead.create({ url: newUrl, platform, userName });
+    else await Lead.create({ url: newUrl, platform: "Instagram", userName });
 
     const leadCounter = await countLeads();
 
     res.status(200).json({ isLead, leadCounter });
   } catch (error) {
-    console.log(error.message);
     res.status(400).json({ error: error.message });
   }
 };
